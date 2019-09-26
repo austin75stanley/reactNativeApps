594b1b26d67cbea6ff7321be7fa289e83bfd991e
austin75stanley <austinstanleybi@gmail.com>
2019-09-26 12:34:08 +0300

    Use of FlexBoxes

--- app/components/Component2/Component2.js
+++ app/components/Component2/Component2.js
@@ -7,9 +7,23 @@ export default class Component2 extends Component {
     render() {
 
         return (
-            <View style={styles.myView}>
-                <Text style={styles.myText}>hello Stanley</Text>
+            <View>
+                <View style={styles.myView}>
+                    <Text style={styles.myText}>hello Stanley</Text>
+                </View>
+                <View style={styles.container}>
+                    <View style={styles.v1}>
+                        <Text>View 1</Text>
+                    </View>
+                    <View style={styles.v2}>
+                        <Text>View 2</Text>
+                    </View>
+                    <View style={styles.v3}>
+                        <Text>View 3</Text>
+                    </View>
+                </View>
             </View>
+
         );
     }
 
@@ -20,7 +34,26 @@ const styles = StyleSheet.create({
     },
     myText: {
         color: 'white'
-    }
+    },
+    container: {
+        flexDirection: 'row',
+        height: 100
+    },
+    v1: {
+        flex: 1,
+        backgroundColor: 'green',
+        padding: 10,
+    },
+    v2: {
+        flex: 1,
+        backgroundColor: 'red',
+        padding: 10,
+    },
+    v3: {
+        flex: 1,
+        backgroundColor: 'yellow',
+        padding: 10,
+    },
 
 })
 AppRegistry.registerComponent('Component2', () => Component2);
