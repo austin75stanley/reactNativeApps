48576b6567f075b328f9ef9043c055eee0ea2b8d
austin75stanley <austinstanleybi@gmail.com>
2019-09-26 12:14:36 +0300

    Inline styling

--- app/components/Component2/Component2.js
+++ app/components/Component2/Component2.js
@@ -1,17 +1,17 @@
 
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
-import Component1 from './app/components/Component1/Component1';
 
-export default class myapp extends Component {
+
+export default class Component2 extends Component {
   render() {
 
     return (
-      <View>
-        <Component1  />
+      <View style={{backgroundColor:'black'}}>
+     <Text style={{color:'red'}}>hello Stanley</Text>   
       </View>
     );
   }
 
 }
-AppRegistry.registerComponent('myapp', () => myapp);
+AppRegistry.registerComponent('Component2', () => Component2);
