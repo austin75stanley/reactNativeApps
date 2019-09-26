7f9799f30be3eb4a329fe6da33d951399176f02c
austin75stanley <austinstanleybi@gmail.com>
2019-09-26 11:08:59 +0300

    React Native First hello World

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+export default class myapp extends Component {
+  render() {
+
+    return(
+    <View>
+      <Text>Hello World</Text>
+    </View>
+    );
+  }
+
+}
+AppRegistry.registerComponent('myapp', () => myapp);
