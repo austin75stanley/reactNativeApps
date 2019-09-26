0981bbb998c7c4b4986a7e8ef8ad7f9d00a5733f
austin75stanley <austinstanleybi@gmail.com>
2019-09-26 11:19:08 +0300

    Passing Props To A component

---
+++ app/components/Component1/Component1.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+
+
+export default class Component1 extends Component {
+  render() {
+
+    return(
+    <View>
+      <Text>{this.props.message}</Text>
+    </View>
+    );
+  }
+
+}
+AppRegistry.registerComponent('Component1', () => Component1);
